4e82b2e151e24deac5be0c9a38812c0eea933714
Matias Correa <matiasecorrea19@gmail.com>
2024-02-15 14:38:10 -0300

    adicionada ordenação, gênero e lingua do filme

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,15 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
-app.get("/movies", async (_, res) => {
-   const movies = await prisma.movie.findMany();
+app.get('/movies', async (_, res) => {
+   const movies = await prisma.movie.findMany({
+     orderBy: { title: 'asc' },
+     include: { genres: true, languages: true },
+   });
    res.json(movies);
-});
+  });
 
 app.listen(port, () => {
    console.log(`Servidor em execução em http://localhost:${port}`);
 });
+
